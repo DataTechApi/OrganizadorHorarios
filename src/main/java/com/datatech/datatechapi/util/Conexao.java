2cdb8da9e2f085e3564189793feef7077765cb7f
Jackson Moraes <jacksontadeu@hotmail.com>
2025-05-25 17:07:14 -0300

    :rocket: Criacao do jar com acesso ao banco de dados na azure

--- src/main/java/com/datatech/datatechapi/util/Conexao.java
+++ src/main/java/com/datatech/datatechapi/util/Conexao.java
@@ -5,10 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String URL = "jdbc:mysql://localhost:3306/datatechapi";
+    //private static final String URL = "jdbc:mysql://localhost:3306/datatechapi";
+    private static final String URL = "jdbc:mysql://datatechapi.mysql.database.azure.com:3306/datatechapi";
 
-    private static String USER = "datatech";
-    private static final String PASSWORD = "admin123";
+    private static String USER = "sr";
+    private static final String PASSWORD = "Admin@123";
 
     public static Connection obterConexao() throws SQLException{
         String driverName = "com.mysql.cj.jdbc.Driver";
